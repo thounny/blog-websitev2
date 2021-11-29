c249f36596b37f95e6522d632b203aadee1601ad
thounny <thounnykeo@gmail.com>
2021-11-29 05:29:51 -0500

    Push new posts on /compose and redirect to home page = logs title and content for blog posts

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const posts = [];
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -26,6 +27,7 @@ app.use(favicon(path.join(__dirname, "/public", "favicon.ico")));
 app.get("/", function(req, res) {
 
     res.render("home", { startingContent: homeStartingContent });
+    console.log(posts);
 });
 
 app.get("/about", function(req, res) {
@@ -41,7 +43,14 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-    console.log(req.body.postBody);
+    const post = {
+        title: req.body.postTitle,
+        content: req.body.postBody
+    };
+
+    posts.push(post);
+
+    res.redirect("/");
 });
 
 
